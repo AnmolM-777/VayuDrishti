76ddb757fe6a3805ab6581075bc143c2ecb7a9cb
AnmolM-777 <b24cs1009@iitj.ac.in>
2026-07-06 18:41:13 +0530

    types: expose all shared types via barrel index export

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Central type exports for VayuDrishti.
+ */
+
+export * from './report';
+export * from './station';
+export * from './hotspot';
+export * from './alert';
+export * from './prediction';
+export * from './user';
